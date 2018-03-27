c0fb353a5d9db8811ffcef01caeb90ef34ad2c44
Jennifer Walker <dataqueen16@gmail.com>
2018-03-27 15:16:30 -0500

    Final resolution for exercise, changing the radius of the object

--- js/circle.js
+++ js/circle.js
@@ -40,9 +40,11 @@
     // console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
+    circle.radius = 5;
+
     // log info about the circle
-    // console.log("Raw circle information");
-    // circle.logInfo(false);
-    // console.log("Circle information rounded to the nearest whole number");
-    // circle.logInfo(true);
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
 })();
