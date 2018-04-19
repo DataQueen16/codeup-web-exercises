641bdd92ac0886c8d589add070a03f367386bb10
Jennifer Walker <dataqueen16@gmail.com>
2018-04-19 14:09:21 -0500

    finished final portion of exercise & corrected functionality

--- js/es6.js
+++ js/es6.js
@@ -29,48 +29,35 @@ const users = [
   }
 ];
 
-// TODO: fill in your name and email and add some programming languages you know
-// to the languages array
-// TODO: replace the `var` keyword with `const`, then try to reassign a variable
-// declared as `const`
 const name = 'jennifer';
 const email = 'dataqueen16@gmail.com';
 const languages = ['java, c++, javascript'];
 
-const name = 'monica';
+// const name = 'monica';
 
-
-// TODO: rewrite the object literal using object property shorthand
 users.push({name, email, languages});
 
-// TODO: replace `var` with `let` in the following variable declarations
 let emails = [];
 let names = [];
 
-// TODO: rewrite the following using arrow functions
-users.forEach = user => emails.push(user.email);
 
-users.forEach = user => names.push(user.name);
-
-
-// TODO: replace `var` with `let` in the following declaration
-let developers = [];
-users.forEach = user =>
-  // TODO: rewrite the code below to use object destructuring assignment
-  //       note that you can also use destructuring assignment in the function
-  //       parameter definition
-  const {name, email, languages} = user;
-
-  // TODO: rewrite the assignment below to use template strings
-  developers.push(name, 's', ' email is', email, name, ' knows ', `{languages.join(', ')}`);
-
-// TODO: Use `let` for the following variable
-var list = '<ul>';
-
-// TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
-
-  // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
+users.forEach(user => {
+    emails.push(user.email); // populate emails
+    names.push(user.name)  // populate names
 });
+
+
+let developers = [];
+
+users.forEach(({name, email, languages}) => {
+  developers.push(`${name}'s email is ${email}. ${name} knows ${languages.join(', ')}`);
+});
+
+
+let list = '<ul>';
+
+
+for (const developer of developers){
+    list += '<li>' + developer + '</li>';
+}
 list += '</ul>';
