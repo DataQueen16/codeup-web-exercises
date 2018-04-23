c23c5a245338e995fabea5a9a68f071aa7592258
Jennifer Walker <dataqueen16@gmail.com>
2018-04-23 10:23:59 -0500

    final solutions to promise exercises

--- js/promises.js
+++ js/promises.js
@@ -16,5 +16,17 @@ function wait(num) {
 wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
+const getGithubUsers = () => {
+    let request = fetch('data/users.json');
+    return request.then(response => response.json())
+};
 
-fetch(url, {headers: {'Authorization': 'token 035d254bd28229cd8a9f7a7d53d9b81b4c101184'}})
+
+
+    const request = fetch('https://api.github.com/users/DataQueen16/events', {headers: {'Authorization': 'token 035d254bd28229cd8a9f7a7d53d9b81b4c101184'}});
+request
+    .then(response => response.json())
+    .then(events => console.log(events[0].created_at))
+;
+
+request.catch(error => console.log(error));
